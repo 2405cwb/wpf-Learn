1777702ad5f550471c21429d7e5a901ffacba140
看看太阳 <aa944589323@gmail.com>
2025-05-19 19:22:36 +0800

    111

--- 平台模板/ReportConversionModule/ViewModels/ReportConversionViewModel.cs
+++ 平台模板/ReportConversionModule/ViewModels/ReportConversionViewModel.cs
@@ -1,7 +1,9 @@
-﻿using Prism.Commands;
+﻿using Microsoft.VisualBasic;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +13,41 @@ namespace ReportConversionModule.ViewModels
 {
     public class ReportConversionViewModel : BindableBase
     {
+        public ReportConversionViewModel()
+        {
+            Message = "View A from your Prism Module";
+
+            for (int i = 0; i < 100; i++)
+            {
+                filePaths.Add("FilePath is :" + i  + ".xlsx");
+            }
+
+        }
         private string _message;
         public string Message
         {
             get { return _message; }
             set { SetProperty(ref _message, value); }
+
+            
         }
 
-        public ReportConversionViewModel()
+        private ObservableCollection<string> filePaths;
+        public ObservableCollection<string> FilePaths
         {
-            Message = "View A from your Prism Module";
-             
+            get { return filePaths; }
+            set { SetProperty(ref filePaths, value); }
         }
+
+        private DelegateCommand _fieldName;
+        public DelegateCommand CommandName =>
+            _fieldName ?? (_fieldName = new DelegateCommand(ExecuteCommandName));
+
+        void ExecuteCommandName()
+        {
+
+        }
+
+      
     }
 }
